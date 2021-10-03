182d45712f519818ab4695cb4da4819181005575
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2021-10-03 18:37:33 +0100

    added requirements and intallations

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Archaea
 Archaea Taxonomic Classification
+
+## INSTALL
+Get PANThER project using:
+```bash
+git clone https://github.com/jorgeMFS/Archaea.git
+chmod +x run.sh
+bash run.sh 
+```
+
+
+## Requirements
+- Ubunto 18.0 or higher
+- Python3.6
+- Anaconda
---
+++ scripts/bash install_tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+Check_Installation() {
+    if ! [ -x "$(command -v $1)" ]; then
+        echo -e "\033[6;31mERROR\033[0;31m: $1 is not installed!" >&2;
+        exit 1;
+    else
+        echo -e "\033[1;32mSUCCESS!\033[0m";
+    fi
+}
+
+echo "Start Installation..."
+
+conda install -c cobilab gto --yes
+conda install -y -c bioconda geco3
+conda install -c bioconda entrez-direct --yes
+conda install -c https://conda.anaconda.org/biocore scikit-bio
+
+#
+pip install openpyxl
+pip install pandas
+pip install numpy
+pip install sklearn
+pip install xgboost
